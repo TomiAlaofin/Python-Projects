c5cda465157d1349dbd0eeb9ace90eb97e5bd7c0
Tomi Alaofin <104176402+TomiAlaofin@users.noreply.github.com>
2023-06-30 14:42:38 -0500

    Update guessing game.py

--- guessing game.py
+++ guessing game.py
@@ -2,12 +2,15 @@ import random
 number = random.randint(1, 20)
 print('Please type your name: ')
 name = input()
+#asks the user for name
 print('hello ' + name + ', would you like to play a guessing game? y/n')
 decision = input()
+#program terminates if the input is 'n' and proceeds if it is 'y' or 'Y'
 if decision == 'Y' or 'y':
     print('lets proceed')
 elif decision == 'n':
     print('Goodbye')
+#returns the user to 'Guess a number when a certain input is entered
 while True:
     for guessesNum in range(1, 7):
         print('Guess a number')
@@ -25,6 +28,7 @@ while True:
         print('The correct number is ' + str(number))
     print('Would you like to play again? y/n')
     playAgain = input()
+#asks user to decide if they want to play again
     if playAgain == 'Y' or 'y':
         continue
     elif playAgain == 'n' or 'N':
