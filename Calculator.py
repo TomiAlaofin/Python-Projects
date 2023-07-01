554225a84f48471cbb309cf33866264eb7aeb374
Tomi Alaofin <104176402+TomiAlaofin@users.noreply.github.com>
2023-07-01 15:50:04 -0500

    Delete Calculator.py

--- Calculator.py
+++
@@ -1,6 +0,0 @@
-num1=input("enter number here:")
-num2= input("enter another number here:")
-# an int function will only allow us to add integers
-# to be able to add both decimal and integers, change the int command to float
-result= float(num1) + float(num2)
-print(result)
