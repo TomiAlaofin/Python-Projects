a44b1fb71f31ebef1e0e2c57adead0c87b91bc14
Tomi Alaofin <104176402+TomiAlaofin@users.noreply.github.com>
2023-07-01 15:47:04 -0500

    Add files via upload

---
+++ Projects.py/guessing game/guessing game.py.py
@@ -0,0 +1,38 @@
+import random
+import sys
+number = random.randint(1, 20)
+print('Please type your name: ')
+name = input()
+# asks the user for name
+print('hello ' + name + ', would you like to play a guessing game? y/n')
+decision = input()
+# program terminates if the input is 'n' and proceeds if it is 'y' or 'Y'
+if decision == 'Y' or 'y':
+    print('lets proceed')
+elif decision == 'n' or 'N':
+    print('Goodbye')
+    sys.exit()
+print('You have a maximum of 7 attempts.')
+# returns the user to 'Guess a number when a certain input is entered
+while True:
+    for guessesNum in range(7):
+        print('Guess a number between 1 and 20')
+        guess = int(input())
+        if guess < number:
+            print('Go higher')
+        elif guess > number:
+            print('Go lower')
+        else:
+            break
+    if guess == number:
+        print('Great job, you guessed the right number in ' +
+              str(guessesNum) + ' attempt(s)')
+    else:
+        print('The correct number is ' + str(number))
+    print('Would you like to play again? y/n')
+    playAgain = input()
+# asks user to decide if they want to play again
+    if playAgain == 'Y' or 'y':
+        break
+    elif playAgain == 'N' or 'n':
+        sys.exit()
